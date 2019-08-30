7242fc054dfc501970fbccc526a5775329dcdadb
jindong <18561604202@163.com>
2019-08-30 14:31:16 +0800

    作者信息页专辑模块完成

--- lib/pages/author/author_issue_page.dart
+++ lib/pages/author/author_issue_page.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_eyepetizer/entity/issue_entity.dart';
+import 'package:flutter_eyepetizer/http/http.dart';
+import 'package:flutter_eyepetizer/pages/author/issue_item_widget.dart';
+import 'package:flutter_eyepetizer/widget/loading_widget.dart';
 
 class AuthorIssuePage extends StatefulWidget {
   final String apiUrl;
@@ -9,12 +15,49 @@ class AuthorIssuePage extends StatefulWidget {
   State<StatefulWidget> createState() => AuthorIssuePageState();
 }
 
-class AuthorIssuePageState extends State<AuthorIssuePage> with AutomaticKeepAliveClientMixin  {
+class AuthorIssuePageState extends State<AuthorIssuePage>
+    with AutomaticKeepAliveClientMixin {
+  List<Item> itemList;
+
+  @override
+  void initState() {
+    super.initState();
+    _getPageData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('专辑'),
-    );
+    return null == itemList
+        ? LoadingWidget()
+        : Container(
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return IssueItemWidget(
+                  item: itemList[index],
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  height: .5,
+                  color: Color(0xFFDDDDDD),
+
+                  /// indent: 前间距, endIndent: 后间距
+                  indent: 15,
+                );
+              },
+              itemCount: itemList.length,
+            ),
+          );
+  }
+
+  void _getPageData() async {
+    HttpUtil.doGet(widget.apiUrl, success: (response) {
+      Map map = json.decode(response.toString());
+      var issue = Issue.fromJson(map);
+      this.setState(() {
+        this.itemList = issue.itemList;
+      });
+    }, fail: (exception) {});
   }
 
   @override
