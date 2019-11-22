8ea26a22959c57e5bd67a43321d25d45ece9104c
ehn <18561604202@163.com>
2019-11-22 08:40:48 +0800

    oktoast 集成

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\flutter\flutter_sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter-project\A-Flutter-Github\flutter_eyepetizer"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/mac/flutter-sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Project-Flutter/Github/flutter_eyepetizer"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=F:\flutter\flutter_sdk\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.0.2"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mac/flutter-sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.4"
 export "FLUTTER_BUILD_NUMBER=1"
