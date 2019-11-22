4c9d5bfbc1e935995508cd09c4c4055d79d844da
ehn <18561604202@163.com>
2019-11-22 14:52:36 +0800

    依赖升级

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,42 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<device_info/DeviceInfoPlugin.h>)
 #import <device_info/DeviceInfoPlugin.h>
+#else
+@import device_info;
+#endif
+
+#if __has_include(<flutter_ijkplayer/IjkplayerPlugin.h>)
 #import <flutter_ijkplayer/IjkplayerPlugin.h>
+#else
+@import flutter_ijkplayer;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<shared_preferences/SharedPreferencesPlugin.h>)
 #import <shared_preferences/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<sqflite/SqflitePlugin.h>)
 #import <sqflite/SqflitePlugin.h>
+#else
+@import sqflite;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
